14a8d0c109e4e657f78e7e8140115f32446da687
pameeeeeee <pammendez5561@gmail.com>
2025-10-07 05:40:01 -0600

    Update S18_A01802629.py

--- S18_A01802629.py
+++ S18_A01802629.py
@@ -1,4 +1,3 @@
-
 temperaturas = [22, 24, 19, 21, 25, 23, 20]
 promedio = sum(temperaturas) / len(temperaturas)
 print(f"Temperatura promedio: {promedio:.2f}°C\n")
@@ -9,3 +8,16 @@ for i, temp in enumerate(temperaturas, start=1):
         print(f"Día {i}: {temp}°C está debajo de la media")
     else:
         print(f"Día {i}: {temp}°C está exactamente en la media")
+
+
+estudiantes = ["Ana", "Luis", "María", "Pedro", "Sofía"]
+promedios = [8.5, 6.0, 9.2, 5.8, 7.0]
+promedio_general = sum(promedios) / len(promedios)
+print(f"Promedio general de los alumnos: {promedio_general:.2f}")
+aprobados = [p for p in promedios if p >= 6.0]
+porcentaje_aprobados = (len(aprobados) / len(promedios)) * 100
+print(f"Porcentaje de alumnos aprobados: {porcentaje_aprobados:.2f}%")
+reprobados = [estudiantes[i] for i, p in enumerate(promedios) if p < 6.0]
+print("Estudiantes que reprobaron:", reprobados)
+
+
